713988aebd9dd08645ef547d005715d4d7fdf61e
ragaashehab <rashehab@yahoo.com>
2022-12-11 18:39:58 +0200

    11 DEC

---
+++ sherlock_array.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    # Write your code here
+    if len(arr)== 1:
+        return "YES"
+    elif len(arr)== 2:
+        return "NO"
+    else:
+        for i in range(len(arr)-1):
+            if sum(arr[:i]) == sum(arr[i+1:]):
+                return "YES"
+        return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
